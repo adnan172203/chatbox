fea9a82fad8c7f8f6dede223b209993b93e2f788
Adnan <adnan172203@gmail.com>
2020-08-19 19:15:32 +0600

    added a function for get user

--- server/users.js
+++ server/users.js
@@ -29,6 +29,9 @@ const removeUser = (id) => {
   if (index !== -1) return users.splice(index, 1)[0];
 };
 
-const getUser = () => {};
+
+//get user
+
+const getUser = (id) => users.find((user) => user.id === id);
 
 const getUsersInRoom = () => {};
