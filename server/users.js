0003385819e6c5a213fd4345de84af4bcaa45fb7
Adnan <adnan172203@gmail.com>
2020-08-27 17:06:30 +0600

    added a function get users in room

--- server/users.js
+++ server/users.js
@@ -34,4 +34,8 @@ const removeUser = (id) => {
 
 const getUser = (id) => users.find((user) => user.id === id);
 
-const getUsersInRoom = () => {};
+//get user room
+const getUsersInRoom = (room) => users.filter((user) => user.room === room);
+
+
+module.exports = { addUser, removeUser, getUser, getUsersInRoom };
