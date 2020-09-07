7431398fecee8649285474d1d00cccef43f7ce00
Adnan <adnan172203@gmail.com>
2020-09-07 15:41:48 +0600

    create message component and import it on messages component

--- client/src/components/Messages/Messages.js
+++ client/src/components/Messages/Messages.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import ScrollToBottom from 'react-scroll-to-bottom';
+import Message from '../Message/Message';
 
+//css
+import './Messages.css';
 
-const Messages = () => {
-    return (
-        <div>
-            
+const Messages = ({ messages, name }) => {
+  return (
+    <ScrollToBottom>
+      {messages.map((message, i) => (
+        <div key={i}>
+          <Message message={message} name={name} />
         </div>
-    )
-}
+      ))}
+    </ScrollToBottom>
+  );
+};
 
 export default Messages;
